60be69ecd69455b69e48ec4b2d5089bbb2d48413
DauletY <erekesh@outlook.comgit config --global user.email erekesh@outlook.comgit config --global user.email erekesh@outlook.com>
2020-08-22 14:48:58 +0500

    c220820

---
+++ Assets/Cs8/Cube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    [SerializeField] float speed;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        Vector3 force = new Vector3(x, 0, y);
+        rb.transform.Translate(force);
+    }
+}
